4b82407fffd3b2de52a8b937646610764ab21ff3
hwigle <jhy7041@gmail.com>
2023-02-06 00:01:05 +0900

    baekjoon 2438 별 찍기 1

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,11 @@ public class Main {
         int N = sc.nextInt();
         sc.close();
 
-        for(int i = N; i>0; i--){
-            System.out.println(i);
+        for(int i = 1; i<=N; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
         }
       
     }
